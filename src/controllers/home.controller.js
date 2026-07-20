5f1bd2c58740a23f2ba82c70b8b239c3ebcdb028
NgoThiMinhQuang <minhquang17012005@gmail.com>
2026-07-20 23:39:36 +0700

    up

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -1,8 +1,17 @@
 import { getHomeData } from "../services/home.service.js";
+import { getAdminSettings } from "../services/admin.service.js";
 
 export async function getHomePage(_request, response, next) {
   try {
     response.json({ success: true, data: await getHomeData() });
+  } catch (error) {
+    next(error);
+  }
+}
+
+export async function getPublicSettings(_request, response, next) {
+  try {
+    response.json({ success: true, data: await getAdminSettings() });
   } catch (error) {
     next(error);
   }
--- src/routes/home.routes.js
+++ src/routes/home.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
 
-import { getHomePage } from "../controllers/home.controller.js";
+import { getHomePage, getPublicSettings } from "../controllers/home.controller.js";
 
 export const homeRouter = Router();
 
 homeRouter.get("/", getHomePage);
+homeRouter.get("/settings", getPublicSettings);
